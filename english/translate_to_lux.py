9b6bdb762bd5b2befe99bd62323ab6e3b881ecf7
Jader <jader.martins@list.lu>
2025-01-27 15:57:53 +0100

    update

--- english/translate_to_lux.py
+++ english/translate_to_lux.py
@@ -18,7 +18,7 @@ def main():
     data = pd.read_csv("data/english_definitions.csv", sep="\t")
     data = data[data.confidence > 0.8]
     tqdm.pandas(desc="Analyzing sentiment")
-    data["lux_definition"] = data["wn_definition"].progress_apply(lambda x: translator(x)[0])
+    data["lux_definition"] = data["wn_definition"].progress_apply(lambda x: translator(x)[0]["translation_text"])
 
     data.dropna().to_csv("data/lod_lux_definitions.csv", sep="\t", index=False)
 
