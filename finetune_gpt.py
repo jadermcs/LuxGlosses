28ad537cda0fbb28aab06ffc6e7291a9fd1c0cf1
Jader <jader.martins@list.lu>
2025-03-05 14:00:35 +0100

    update

--- finetune_gpt.py
+++ finetune_gpt.py
@@ -1,14 +1,33 @@
 from openai import OpenAI
+import random
+from datasets import load_dataset
 import os
 
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
+letz = load_dataset("fredxlpy/LETZ", "LETZ-SYN")
 
-client.fine_tuning.jobs.create(
-    training_file="file-abc123",
-    model="gpt-4o-mini-2024-07-18"
-)
+words = letz["train"]["label"]
 
-file_id = response["id"]
-print("Uploaded file ID:", file_id)
+NUM = 4
+
+for example in letz["train"]:
+    choices = random.choices(words, k=NUM)
+    i = random.randint(0, NUM-1)
+    word = example["label"]
+    choices[i] = word
+    options = "\n".join([f"{x}) {choices[idx]}" for idx, x in enumerate("ABCD")])
+    json = {"messages": [
+        {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish meaning."},
+        {"role": "user", "content": f"What synonym word is contained in '{example['text']}': \n{options}"}
+        {"role": "assistant", "content": f"{word}"}
+        ]}
+    print(json)
+    break
+
+# response = client.fine_tuning.jobs.create(
+#     training_file="file-abc123",
+#     model="gpt-4o-mini-2024-07-18"
+# )
+
 
