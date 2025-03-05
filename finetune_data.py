e9f76b3a29b22c769338882561e9c7c3565137ea
Jader <jader.martins@list.lu>
2025-03-05 16:54:33 +0100

    update

--- finetune_data.py
+++ finetune_data.py
@@ -1,7 +1,9 @@
 import json
 import pandas as pd
 
-df = pd.read_csv("data/lod_english_word.csv", sep="\t").sample(frac=1.)
+df = pd.read_csv("data/lod_english_word.csv", sep="\t")
+df = df.dropna(subset="en_word")
+df = df.sample(frac=1.)
 
 test = open("test.jsonl", "w")
 
@@ -9,16 +11,17 @@ for mode in ["train", "test"]:
     filtered = df.lemma <= "J"
     if mode == "test":
         filtered != filtered
+    df_group = df[filtered].groupby("en_word").first().reset_index()
     with open(f"{mode}.jsonl", "w") as fout:
         count = 0
-        for idx, row in df[df.lemma <= "J"].groupby("en_word").first().reset_index().iterrows():
+        for idx, row in df_group.iterrows():
             count += 1
             word = row["en_word"]
             lux_word = row["lemma"]
             data = {"messages": [
                 {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish language."},
-                {"role": "user", "content": f"In the sentence '{row['sentence']}' what is the meaning of '{lux_word}', answer with maximum 5 words."},
-                {"role": "assistant", "content": f"{word}"}
+                {"role": "user", "content": f"In the sentence '{row['sentence']}' what word means '{word}'. Give me the word in it's base form (lemma)."},
+                {"role": "assistant", "content": f"{lux_word}"}
                 ]}
             fout.write(json.dumps(data)+"\n")
             if count >= 1000:
