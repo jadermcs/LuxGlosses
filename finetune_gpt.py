d5269446124104e1fe2aaaf410ae6087f5172d86
Jader <jader.martins@list.lu>
2025-03-05 14:06:25 +0100

    update

--- finetune_gpt.py
+++ finetune_gpt.py
@@ -1,7 +1,8 @@
-from openai import OpenAI
-import random
-from datasets import load_dataset
 import os
+import json
+import random
+from openai import OpenAI
+from datasets import load_dataset
 
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
@@ -11,19 +12,19 @@ words = letz["train"]["label"]
 
 NUM = 4
 
-for example in letz["train"]:
-    choices = random.choices(words, k=NUM)
-    i = random.randint(0, NUM-1)
-    word = example["label"]
-    choices[i] = word
-    options = "\n".join([f"{x}) {choices[idx]}" for idx, x in enumerate("ABCD")])
-    json = {"messages": [
-        {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish meaning."},
-        {"role": "user", "content": f"What synonym word is contained in '{example['text']}': \n{options}"},
-        {"role": "assistant", "content": f"{word}"}
-        ]}
-    print(json)
-    break
+with open("train.json", "w") as fout:
+    for example in letz["train"]:
+        choices = random.choices(words, k=NUM)
+        i = random.randint(0, NUM-1)
+        word = example["label"]
+        choices[i] = word
+        options = "\n".join([f"{x}) {choices[idx]}" for idx, x in enumerate("ABCD")])
+        data = {"messages": [
+            {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish meaning."},
+            {"role": "user", "content": f"What synonym word is contained in '{example['text']}': \n{options}"},
+            {"role": "assistant", "content": f"{'ABCD'[i]}) {word}"}
+            ]}
+        fout.write(json.dumps(data)+"\n")
 
 # response = client.fine_tuning.jobs.create(
 #     training_file="file-abc123",
