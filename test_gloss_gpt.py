4f7943bca0d9fc7e4221932053c7088c3374f768
Jader Martins <jader.martins@list.lu>
2025-03-07 12:57:58 +0000

    update

--- test_gloss_gpt.py
+++ test_gloss_gpt.py
@@ -15,20 +15,25 @@ for model_name in ["gpt-4o-mini", "ft:gpt-4o-mini-2024-07-18:list:lod-translate:
     for idx, row in glosses.iterrows():
         sentence = row["usage"]
         word = row["word"]
-        data = {"messages": [
+        data = [
             {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish language."},
-            {"role": "user", "content": f"In the sentence '{sentence}' what is the dictionary meaning of '{word}'. After reasoning give the answer in the last line."},
-            ]}
+            {"role": "user", "content": f"In the sentence '{sentence}' what is the definition of '{word}'? Give the definition in Luxembourgish. After reasoning give the answer in the last line."},
+            ]
+        print(data)
         completion = client.chat.completions.create(
             model=model_name,
             messages=data
         )
 
         response = completion.choices[0].message.content.split("\n")[-1]
+        print(response)
         pred.append(response)
-        if row["usage2"]:
-            ref.append([row["usage1"], row["usage2"]])
+        if row["definition2"]:
+            ref.append([row["definition1"], row["definition2"]])
         else:
-            ref.append([row["usage1"]])
+            ref.append([row["definition1"]])
+        break
 
-    print(bleu.compute(pred, ref))
+    results = bleu.compute(predictions=pred, references=ref)
+    print(results)
+
