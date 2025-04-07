78e3dc3e1416f9fcfd8f36a3e93b13df3ced69d4
Jader <jader.martins@list.lu>
2025-04-07 19:04:59 +0200

    update

--- test_gloss_gpt.py
+++ test_gloss_gpt.py
@@ -5,18 +5,19 @@ from tqdm import tqdm
 
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-glosses = pd.read_csv("glosses.tsv", sep="\t")
+glosses = pd.read_csv("data/english_definitions.csv", sep="\t")
+glosses = glosses[glosses.confidence > 0.6].groupby("wn_definition").first().reset_index()
+print(glosses)
 
 for model_name in ["ft:gpt-4o-mini-2024-07-18:list:lets-phrase:B9V61v1e"]:
     ref = []
     pred = []
     count = 0
     for idx, row in tqdm(glosses.iterrows(), total=glosses.shape[0]):
-        sentence = row["usage"]
-        word = row["word"]
+        sentence = row["wn_definition"]
         data = [
             {"role": "system", "content": "You are a helpful assistant specialized in Luxembourgish language."},
-            {"role": "user", "content": f"In the sentence '{sentence}' what is the definition of '{word}'? Give the definition in Luxembourgish the definition should not contain the word itself. After reasoning give the answer in the last line."},
+            {"role": "user", "content": f"Translate the sentence '{sentence}' to Luxembourgish."},
             ]
         completion = client.chat.completions.create(
             model=model_name,
@@ -24,9 +25,10 @@ for model_name in ["ft:gpt-4o-mini-2024-07-18:list:lets-phrase:B9V61v1e"]:
         )
 
         response = completion.choices[0].message.content.split("\n")[-1].split(":")[-1]
-        pred.append((word, response))
+        pred.append((row["sentence"], sentence, row["confidence"], response))
         count += 1
 
-    with open(f"{model_name}.txt", "w") as fout:
-        for word, definition in pred:
-            fout.write(f"{word}\t{definition}\n")
+    with open(f"{model_name}_definition.txt", "w") as fout:
+        for element in pred:
+            element = "\t".join(element) + "\n"
+            fout.write(element)
