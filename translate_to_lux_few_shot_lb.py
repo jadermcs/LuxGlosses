da0750212e512efd8289c276e1947fee7528fd13
Fred Philippy <fred@zortify.com>
2025-04-14 14:56:48 +0200

    update

--- translate_to_lux_few_shot_lb.py
+++ translate_to_lux_few_shot_lb.py
@@ -32,12 +32,12 @@ few_shot_samples = pd.DataFrame({
     })
 
 model_inputs = [
-    {"role": "system", "content": "You are a helpful assistant that translates glosses from English to Luxembourgish."}
+    {"role": "system", "content": "Du bass en hëllefsbereeten Assistent, deen Definitiounen aus dem Engleschen op Lëtzebuergesch iwwersetzt."}
 ]
 
 for j in range(len(few_shot_samples)):
-    model_inputs.append({"role": "user", "content": f"English word: {few_shot_samples.loc[j, 'word_en']}\nLuxembourgish word translation: {few_shot_samples.loc[j, 'word_lb']}\nLuxembourgish word in example sentence: {few_shot_samples.loc[j, 'usage']}\nEnglish word definition: {few_shot_samples.loc[j, 'definition_en']}"})
-    model_inputs.append({"role": "assistant", "content": f"Luxembourgish translation of definition: {few_shot_samples.loc[j, 'definition_lb']}"})
+    model_inputs.append({"role": "user", "content": f"Englescht Wuert: {few_shot_samples.loc[j, 'word_en']}\nLëtzebuergesch Iwwersetzung vum Wuert: {few_shot_samples.loc[j, 'word_lb']}\nLëtzebuergescht Wuert an engem Beispillsaz: {few_shot_samples.loc[j, 'usage']}\nEnglesch Definitioun vum Wuert: {few_shot_samples.loc[j, 'definition_en']}"})
+    model_inputs.append({"role": "assistant", "content": f"Lëtzebuergesch Iwwersetzung vun der Definitioun: {few_shot_samples.loc[j, 'definition_lb']}"})
 
 # Load data
 data = pd.read_csv('data/english_definitions.csv', delimiter='\t')
@@ -45,13 +45,13 @@ data = data[data['confidence']>=0.8]
 
 for i, row in data[:50].iterrows():
 
-    messages = model_inputs + [{"role": "user", "content": f"English word: {row['en_word']}\nLuxembourgish word translation: {row['lemma']}\nLuxembourgish word in example sentence: {row['sentence']}\nEnglish word definition: {row['wn_definition']}"}]
+    messages = model_inputs + [{"role": "user", "content": f"Englescht Wuert: {row['en_word']}\nLëtzebuergesch Iwwersetzung vum Wuert: {row['lemma']}\nLëtzebuergescht Wuert an engem Beispillsaz: {row['sentence']}\nEnglesch Definitioun vum Wuert: {row['wn_definition']}"}]
 
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=messages).choices[0].message.content
     
-    lux_definition = response.replace('Luxembourgish translation of definition: ', '')
+    lux_definition = response.replace('Lëtzebuergesch Iwwersetzung vun der Definitioun: ', '')
     
     data.loc[i, 'lux_definition'] = lux_definition
 
@@ -60,7 +60,7 @@ for i, row in data[:50].iterrows():
     print('English definition: ', row['wn_definition'])
     print('Luxembourgish translation: ', lux_definition)
 
-data.dropna().to_csv("data/lod_lux_definitions_fewshot_v3.csv", sep="\t", index=False)
+data.dropna().to_csv("data/lod_lux_definitions_fewshot_v2.csv", sep="\t", index=False)
 
 
 
